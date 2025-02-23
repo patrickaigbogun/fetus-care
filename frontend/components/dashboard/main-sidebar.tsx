6a1a3e4b72abd364a01b516f066394486ea8371e
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2025-02-23 10:32:32 +0100

    working on header

--- frontend/components/dashboard/main-sidebar.tsx
+++ frontend/components/dashboard/main-sidebar.tsx
@@ -11,6 +11,8 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { List } from "@phosphor-icons/react";
+import { Flex, IconButton } from "@radix-ui/themes";
 
 const sidebarStuff = [
   {
@@ -44,52 +46,45 @@ const MainSidebar = () => {
   const pathname = usePathname();
   const [isExpanded, setIsExpanded] = useState(false);
 
-  return (
-    <div
-      className={`${
-        isExpanded ? "w-64" : "w-[5dvw]"
-      } transition-all duration-300 ease-in-out border-r h-screen sticky top-0 z-10 p-1 flex flex-col justify-start gap-6 bg-purple-200/60`}
-    >
-      <button
-        onClick={() => setIsExpanded(!isExpanded)}
-        className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-neutral-900 p-1 aspect-square w-fit rounded-2xl"
-      >
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          width={44}
-          className="max-w-[32px]"
-          height={44}
-        />
-      </button>
-      <div className="flex flex-col gap-2">
-        {sidebarStuff.map((item) => (
-          <Link key={item.label} href={item.href} className="w-full">
-            <button
-              className={`items-center  text-sm font-medium w-full flex align-middle place-items-center  rounded-2xl transition-all duration-300 ease-in-out ${
-                !isExpanded
-                  ? "aspect-square max-w-[50px] justify-center gap-2 p-2"
-                  : " justify-start gap-4 p-2"
-              } ${
-                pathname === item.href
-                  ? "bg-purple-200 text-purple-900"
-                  : "text-neutral-900 hover:text-neutral-900 hover:bg-purple-200"
-              }`}
-            >
-              <item.icon className="h-6 w-6" />
-              <span
-                className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                  isExpanded ? "opacity-100  flex" : "hidden opacity-0"
-                }`}
-              >
-                {item.label}
-              </span>
-            </button>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`${isExpanded ? "w-64" : "w-[5dvw]"
+				} transition-all duration-300 ease-in-out border-r h-screen sticky top-0 z-10 p-1 flex flex-col justify-start gap-6 bg-purple-200/60`}
+		>
+			<div className="flex flex-col justify-center p-2">
+			<IconButton
+			variant='soft'
+			color='purple'
+				onClick={() => setIsExpanded(!isExpanded)}
+			>
+				<List size={28} weight='duotone'/>
+			</IconButton>
+			<div className="flex flex-col gap-2">
+				{sidebarStuff.map((item) => (
+					<Link key={item.label} href={item.href} className="w-full">
+						<button
+							className={`items-center  text-sm font-medium w-full flex align-middle place-items-center  rounded-2xl transition-all duration-300 ease-in-out ${!isExpanded
+									? "aspect-square max-w-[50px] justify-center gap-2 p-2"
+									: " justify-start gap-4 p-2"
+								} ${pathname === item.href
+									? "bg-purple-200 text-purple-900"
+									: "text-neutral-900 hover:text-neutral-900 hover:bg-purple-200"
+								}`}
+						>
+							<item.icon className="h-6 w-6" />
+							<span
+								className={`overflow-hidden transition-all duration-300 ease-in-out ${isExpanded ? "opacity-100  flex" : "hidden opacity-0"
+									}`}
+							>
+								{item.label}
+							</span>
+						</button>
+					</Link>
+				))}
+			</div>
+			</div>
+		</div>
+	);
 };
 
 export default MainSidebar;
